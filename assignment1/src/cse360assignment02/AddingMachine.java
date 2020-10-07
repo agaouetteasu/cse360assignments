3be066bb23f27d8aad247c2dbe6f1e81be1e3be1
Alex Gaouette <agaouett@asu.edu>
2020-10-07 14:40:41 -0700

    Implemented previously empty methods

--- assignment1/src/cse360assignment02/AddingMachine.java
+++ assignment1/src/cse360assignment02/AddingMachine.java
@@ -8,56 +8,68 @@
 package cse360assignment02;
 
 /**
- * Implementation of an adding machine capable
- * adding and subtracting integers.
+ * Implementation of an adding machine capable of adding/subtracting integers.
+ * @author Alex Gaouette <agaouett@asu.edu>
  */
 public class AddingMachine {
 	
   /**
-   * Used for keeping track of the running total
-   * of integers added
+   * Used for keeping track of the running total of all operations.
    */
   private int total;
+  /**
+   * Used for keeping track of the operation history of the adding machine.
+   */
+  private String history;
   
   /**
    * Constructor for the AddingMachine class.
    */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "" + total;
   }
   
   /**
-   * Used to get the current total
+   * Used to get the current total.
    * @return total - current total
    */
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   /**
-   * Adds an integer to the current total
+   * Adds an integer to the current total.
+   * Updates the history of the adding machine to include the new operation.
    * @param value - an integer value to be added to the current total
    */
   public void add (int value) {
+	  total += value;
+	  history += " + " + value;
   }
   
   /**
-   * Subtracts an integer from the current total
+   * Subtracts an integer from the current total.
+   * Updates the history of the adding machine to include the new operation.
    * @param value - an integer value to be subtracted from the current total
    */
   public void subtract (int value) {
+	  total -= value;
+	  history += " - " + value;
   }
   
   /**
-   * Returns the history of adding machine operations
+   * Returns the history of adding machine operations.
    */
   public String toString () {
-    return "";
+    return history;
   }
   
   /**
-   * Clears the adding machine history and total
+   * Clears the adding machine history and total.
    */
   public void clear() {
+	  total = 0;
+	  history = "";
   }
 }
